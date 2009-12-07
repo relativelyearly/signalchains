9d6c3227cb563a832c130f7d1197f5227f356be6
Ross Brown <albatrocity@gmail.com>
2009-12-06 20:42:00 -0600

    Attempting jplayer, getting errors on progress change

--- app/helpers/chains_helper.rb
+++ app/helpers/chains_helper.rb
@@ -22,24 +22,29 @@ module ChainsHelper
       link_to "<span>#{chain.likes.size}</span>", like_chain_path(chain), :id => 'like'
     end
   end
+  
+  def comments_link(chain)
+    link_to "<span>#{chain.comments.size}</span>", '#comments', :id => 'comments_btn'
+  end
 
   def complete_message
     capture_haml do
       haml_tag(:ul, :class => 'incomplete_chain') do
         unless @chain.audio.high_quality.file? && @chain.input_source? && @chain.preamp?
-          haml_tag(:h2, 'This chain is incomplete.')
+          haml_tag(:h4, 'This chain is incomplete')
+          haml_tag(:p, 'In order for others to see your chain, you need to finish creating it.')          
         end
 
         unless @chain.audio.high_quality.file?
-          haml_tag(:li, "It needs an audio file.")
+          haml_tag(:li, "It needs an <a href='#audio'>audio file</a>.")
         end
 
         unless @chain.input_source?
-          haml_tag(:li, "It needs an input source.")
+          haml_tag(:li, "It needs an <a href='#input_sources'>input source</a>.")
         end
 
         unless @chain.preamp?
-          haml_tag(:li, "It needs a preamp.")
+          haml_tag(:li, "It needs a <a href='#extra_gear'>preamp</a>.")
         end
       end
     end
--- public/javascripts/rogue/page.chain.js
+++ public/javascripts/rogue/page.chain.js
@@ -9,6 +9,118 @@ $(function() {
         'imageScale': true
       });
 });
+
+var global_lp = 0;
+
+
+$("#pause").hide();
+
+function showPauseBtn()
+{
+  $("#play").fadeOut(function(){
+    $("#pause").fadeIn();
+  });
+}
+
+function showPlayBtn()
+{
+  $("#pause").fadeOut(function(){
+    $("#play").fadeIn();
+  });
+}
+
+function playTrack(t,n)
+{
+  $("#jquery_jplayer").setFile(t).play();
+
+  showPauseBtn();
+
+  return false;
+}
+
+$("#play").click(function() {
+  $("#jquery_jplayer").play();
+  showPauseBtn();
+  return false;
+});
+
+$("#pause").click(function() {
+  $("#jquery_jplayer").pause();
+  showPlayBtn();
+  return false;
+});
+
+$("#stop").click(function() {
+  $("#jquery_jplayer").stop();
+  showPlayBtn();
+  return false;
+});
+
+
+$("#volume-min").click( function() {
+  $('#jquery_jplayer').volume(0);
+  $('#sliderVolume').slider('option', 'value', 0);
+  return false;
+});
+
+$("#volume-max").click( function() {
+  $('#jquery_jplayer').volume(100);
+  $('#sliderVolume').slider('option', 'value', 100);
+  return false;
+});
+
+$("#player_progress_ctrl_bar a").live( "click", function() {
+  $("#jquery_jplayer").playHead(this.id.substring(3)*(100.0/global_lp));
+  return false;
+});
+
+
+$("#jquery_jplayer").onProgressChange( function(lp,ppr,ppa,pt,tt) {
+  var lpInt = parseInt(lp);
+  var ppaInt = parseInt(ppa);
+  global_lp = lpInt;
+
+  $('#loaderBar').progressbar('option', 'value', lpInt);
+  $('#sliderPlayback').slider('option', 'value', ppaInt);
+});
+
+
+// Slider
+$('#sliderPlayback').slider({
+  max: 100,
+  range: 'min',
+  animate: true,
+
+  slide: function(event, ui)
+  {
+    $("#jquery_jplayer").playHead(ui.value*(100.0/global_lp));
+  }
+});
+
+$('#sliderVolume').slider({
+  value : 50,
+  max: 100,
+  range: 'min',
+  animate: true,
+
+  slide: function(event, ui)
+  {
+    $("#jquery_jplayer").volume(ui.value);
+  }
+});
+
+$('#loaderBar').progressbar();
+
+
+//hover states on the static widgets
+$('#dialog_link, ul#icons li').hover(
+  function() { $(this).addClass('ui-state-hover'); },
+  function() { $(this).removeClass('ui-state-hover'); }
+);
+
+
+
+
 });
 
 function toggle_edit_link (element) {
