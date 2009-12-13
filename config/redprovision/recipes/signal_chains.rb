09e4440624cdd838aaf33f814a3f24637436f587
Matt Pruitt <guitsaru@gmail.com>
2009-12-12 18:21:05 -0600

    Recipe stuff.

--- config/redprovision/recipes/signal_chains.rb
+++ config/redprovision/recipes/signal_chains.rb
@@ -48,12 +48,14 @@ class SignalChains
     :group => 'www-data')
 
   timestamp = DateTime.now.strftime("%G%m%d%H%M")
+  run "cd /u/apps/signalchains"
+  run "cd /u/apps/signalchains", :user => 'deploy'
   run "ssh -o StrictHostKeyChecking=no git@github.com"
   run "git clone -q --depth 1 git@github.com:relativelyearly/signalchains.git /u/apps/signalchains/releases/#{timestamp}"
   run "chown -R deploy:www-data /u/apps/signalchains"
   run "ln -s /u/apps/signalchains/releases/#{timestamp} /u/apps/signalchains/current", :user => 'deploy'
   run "ln -s /u/apps/signalchains/shared/config/database.yml /u/apps/signalchains/current/config/database.yml", :user => 'deploy'
-  run "rake -f /u/apps/signalchains/releases/#{timestamp}/Rakefile gems:install RAILS_ENV=\#{environment == Staging ? 'staging' : 'production'}", :user => 'deploy'
+  run "cd /u/apps/signalchains && rake -f /u/apps/signalchains/releases/#{timestamp}/Rakefile gems:install RAILS_ENV=\#{environment == Staging ? 'staging' : 'production'}", :user => 'deploy'
 
   template({'signalchains.erb' => '/etc/apache2/sites-available/signalchains'},
     :user => 'deploy',
--- config/redprovision/recipes/signal_chains_database.rb
+++ config/redprovision/recipes/signal_chains_database.rb
@@ -1,6 +1,7 @@
 class SignalChainsDatabase
   include Redprovision::Recipe
 
-  run "cd /u/apps/signalchains/current"
-  run "rake -f /u/apps/signalchains/current/Rakefile db:migrate RAILS_ENV=\#{environment == Staging ? 'staging' : 'production'}", :user => 'deploy'
+  run "cd /u/apps/signalchains && rake -f /u/apps/signalchains/current/Rakefile db:migrate RAILS_ENV=\#{environment == Staging ? 'staging' : 'production'}", :user => 'deploy'
+  run "mkdir /u/apps/signalchains/current/tmp", :user => 'deploy'
+  run "touch /u/apps/signalchains/current/tmp/restart.txt", :user => 'deploy'
 end
