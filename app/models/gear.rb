7bcfa902370ce0354a929541259932ebe3095547
Matt Pruitt <guitsaru@gmail.com>
2009-12-23 21:00:50 -0600

    Don't show non featured gear.

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -18,6 +18,7 @@ class Gear < ActiveRecord::Base
       :class => [Mic, Preamp, Equalizer, DynamicsProcessor, EffectsProcessor],
       :page => 1,
       :per_page => 1,
+      :without => {:featured_at => nil},
       :order => 'featured_at DESC').first
   end
 
