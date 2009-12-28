4b64614045ead30b111c5d33b3c48f8b83feb7c5
Matt Pruitt <guitsaru@gmail.com>
2009-12-28 10:37:36 -0600

    Change ad style.

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -15,5 +15,5 @@
 class Ad < ActiveRecord::Base
   attr_accessible :image, :url
 
-  has_attached_file(:image, :styles => {:display => ['130x100#', :jpg]})
+  has_attached_file(:image, :styles => {:display => '130x100#'})
 end
