c6b46600dac0557a765723947ac95a1fcb34fe4c
Matt Pruitt <guitsaru@gmail.com>
2010-02-11 14:23:33 -0600

    Use correct path for authorized keys.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+default_run_options[:pty] = true
+
 set :application, "signalchains"
 set :repository,  "git@github.com:relativelyearly/signalchains.git"
 set :user, "deploy"
@@ -9,6 +11,8 @@ set :host, '69.164.194.118'
 role :app, host
 role :web, host
 role :db,  host
+
+ssh_options[:port] = 30000
 
 set :backup_database_before_migrations, false
 set :disable_web_during_migrations,     true
