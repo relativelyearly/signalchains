041b77683e61497e2c2da515d0778f6899ea21e4
Matt Pruitt <guitsaru@gmail.com>
2010-02-24 14:00:50 -0600

    Try to fix paperclip.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,12 +7,12 @@ set :deploy_via, :remote_cache
 set :scm, :git
 set :deploy_to, '/data/signalchains'
 
-set :host, '69.164.194.118'
+set :host, '72.14.191.62'
 role :app, host
 role :web, host
 role :db,  host
 
-ssh_options[:port] = 30000
+# ssh_options[:port] = 30000
 
 set :backup_database_before_migrations, false
 set :disable_web_during_migrations,     true
